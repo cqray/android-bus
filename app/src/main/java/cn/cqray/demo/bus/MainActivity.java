b7ef36433bee3b2eeb39b4de8290a88fd6d60787
LeiJue <tapmail@qq.com>
2020-04-01 21:58:37 +0800

    commit

--- app/src/main/java/cn/cqray/demo/bus/MainActivity.java
+++ app/src/main/java/cn/cqray/demo/bus/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import cn.cqray.android.bus.Rxbus;
+import io.reactivex.functions.Consumer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Rxbus.setDebug(true);
+
+        // 注册Rxbus
+        Rxbus.with(this)
+                .of(String.class)
+                .tag("666")
+                .sticky()
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+
+                    }
+                });
+
+        Rxbus.post("123");
     }
 }
